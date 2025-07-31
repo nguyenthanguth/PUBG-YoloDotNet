9256225df65af3043bd71a4fff83415d8f0f0a0c
Nguyen Thang <nguyenthanguth@gmail.com>
2025-07-31 17:53:32 +0700

    250731

---
+++ auto-aim/fMain.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace auto_aim
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btStart = new Button();
+            SuspendLayout();
+            // 
+            // btStart
+            // 
+            btStart.Location = new Point(664, 393);
+            btStart.Name = "btStart";
+            btStart.Size = new Size(124, 45);
+            btStart.TabIndex = 0;
+            btStart.Text = "START";
+            btStart.UseVisualStyleBackColor = true;
+            btStart.Click += this.btStart_Click;
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btStart);
+            Name = "fMain";
+            Text = "YoloDotnet";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btStart;
+    }
+}
---
+++ auto-aim/fMain.cs
@@ -0,0 +1,17 @@
+using System.Drawing;
+
+namespace auto_aim
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("OK");
+        }
+    }
+}
